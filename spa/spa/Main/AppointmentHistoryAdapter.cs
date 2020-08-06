a1f575e27ae1e7b950b8c72005803fd0479d1118
Phan Việt Đức <40122551+pvduc271099@users.noreply.github.com>
2020-08-06 21:13:11 +0700

    Appointment history classes

---
+++ spa/spa/Main/AppointmentHistoryAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Java.Util.Zip;
+
+namespace AppointmentHustory
+{
+    class AppointmentHistoryAdapter : RecyclerView.Adapter
+    {
+        List<Appointment> mList;
+        Context mContext;
+
+        public AppointmentHistoryAdapter(List<Appointment> list, Context con)
+        {
+            mList = list;
+            mContext = con;
+        }
+
+        public override int ItemCount
+        {
+            get { return mList.Count; }
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            Myview mHolder = holder as Myview;
+            mHolder.date.Text = mList[position].mDate;
+            mHolder.total.Text = mList[position].mTotal;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            Context context = parent.Context;
+            LayoutInflater layoutInflater = LayoutInflater.From(context);
+
+            View AppointmentsView = layoutInflater.Inflate(Resource.Layout.appointment_item, parent, false);
+            Myview mHolder = new Myview(AppointmentsView);
+            return mHolder;
+        }
+
+        public class Myview : RecyclerView.ViewHolder
+        {
+            private View itemView;
+            public TextView date, total;
+            public ImageView detail;
+
+            public Myview(View _itemView) : base(_itemView)
+            {
+                date = _itemView.FindViewById<TextView>(Resource.Id.txtDate);
+                total = _itemView.FindViewById<TextView>(Resource.Id.txtTotal);
+                detail = _itemView.FindViewById<ImageView>(Resource.Id.btnDetail);
+
+                detail.Click += delegate { DetailButtonClick(_itemView); };
+            }
+
+            public void DetailButtonClick(View view)
+            {
+                string test = "Detail button click";
+                Toast.MakeText(view.Context, test, ToastLength.Short).Show();
+            }
+
+            
+        }
+    }
+}
---
+++ spa/spa/Main/Appointment_history.cs
@@ -0,0 +1,104 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+using Android.Text;
+using System;
+
+namespace AppointmentHustory
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public RecyclerView recyclerView;
+        List<Appointment> appointmentsList;
+        AppointmentHistoryAdapter adapter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbarAppointmentHistory);
+            SetSupportActionBar(toolbar);
+            toolbar.SetNavigationIcon(Resource.Drawable.abc_ic_ab_back_material);
+            toolbar.NavigationClick += delegate { backButtonClick(); };
+
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.AppointmentList);
+
+            string mDate = "2020/03/17";
+            string mOutDate;
+            FormatDateString(mDate, out mOutDate);
+            appointmentsList = new List<Appointment>();
+            appointmentsList.Add(new Appointment(mOutDate, ChangeIntToStringComma(15000000)));
+            appointmentsList.Add(new Appointment(mOutDate, ChangeIntToStringComma(15000000)));
+            appointmentsList.Add(new Appointment(mOutDate, ChangeIntToStringComma(15000000)));
+
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+            adapter = new AppointmentHistoryAdapter(appointmentsList, this);
+            recyclerView.SetAdapter(adapter);
+            recyclerView.SetLayoutManager(linearLayoutManager);
+        }
+
+        public void backButtonClick()
+        {
+            Android.Widget.Toast.MakeText(this, "Back button clicked", Android.Widget.ToastLength.Long).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public string ChangeIntToStringComma(int a)
+        {
+            string input = a.ToString();
+            List<char> re = new List<char>();
+            for (int j = 1, i = input.Length - 1; i >= 0; ++j)
+            {
+                if (j % 4 == 0)
+                {
+                    //re.Add(' ');
+                    re.Add(',');
+                    //re.Add(' ');
+                    continue;
+                }
+                re.Add(input[i]);
+                i--;
+            }
+
+            char[] charArray = re.ToArray();
+            Array.Reverse(charArray);
+            string result = new string(charArray);
+            return result;
+        }
+
+        public string ReverseString(string input)
+        {
+            char[] tmp = input.ToCharArray();
+            Array.Reverse(tmp);
+            return new string(tmp);
+        }
+
+
+        public bool FormatDateString(string inputDate, out string outputdate)
+        {
+            DateTime dateValue;
+            if (DateTime.TryParse(inputDate, out dateValue))
+            {
+                dateValue = DateTime.Parse(inputDate);
+                outputdate = dateValue.ToString("dd/MM/yyyy");
+                return true;
+            }
+            outputdate = null;
+            return false;
+        }
+    }
+}
